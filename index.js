f6a9e6d0be6d41d2c8a374beaab7c5f1d24e2a70
SergeyB <brynkin.dev@gmail.com>
2023-11-14 15:15:13 +0300

    add modal prototype

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-scrollHandler = () => {
+const setHeaderStyle = (style) => {
   const header = document.querySelector("header");
   const logo = document.querySelector(".main-logo");
   const menu = document.querySelector(".icon-menu_mobile");
 
-  if (window.pageYOffset > 0) {
-    header.classList.remove("bg-color-blue1");
-    header.classList.add("bg-color-white");
-
-    logo.src = "src/imgs/logo_mobile_blue.svg";
-    menu.src = "src/imgs/icon_menu_mobile_gray.svg";
-  } else {
+  if (style === "blue") {
     header.classList.remove("bg-color-white");
     header.classList.add("bg-color-blue1");
 
     logo.src = "src/imgs/logo_mobile_white.svg";
     menu.src = "src/imgs/icon_menu_mobile_white.svg";
+  }
+
+  if (style === "white") {
+    header.classList.remove("bg-color-blue1");
+    header.classList.add("bg-color-white");
+
+    logo.src = "src/imgs/logo_mobile_blue.svg";
+    menu.src = "src/imgs/icon_menu_mobile_gray.svg";
+  }
+};
+
+const scrollHandler = () => {
+  if (window.pageYOffset > 0) {
+    setHeaderStyle("white");
+  } else {
+    setHeaderStyle("blue");
+  }
+};
+
+const clickMenuButtonHandler = () => {
+  const modal = document.querySelector(".modal-menu");
+
+  if (modal.style.display === "none") {
+    modal.style.setProperty("display", "block");
+    setHeaderStyle("white");
+    window.removeEventListener("scroll", scrollHandler);
+  } else {
+    modal.style.setProperty("display", "none");
+    setHeaderStyle("blue");
+    window.addEventListener("scroll", scrollHandler);
   }
 };
 
