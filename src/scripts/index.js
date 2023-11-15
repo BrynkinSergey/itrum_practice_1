c635c0f8028f2681ddcfdb885b7313e72a4b8e1a
SergeyB <brynkin.dev@gmail.com>
2023-11-15 13:16:57 +0300

    refactor all mobile 1st section for adaptive layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,30 +1,34 @@
 const setHeaderStyle = (style) => {
   const header = document.querySelector("header");
-  const logo = document.querySelector(".main-logo");
+  const logoMobile = document.querySelector(".main-logo--mobile");
+  const logoTablet = document.querySelector(".main-logo--tablet");
   const menu = document.querySelector(".icon-menu_mobile");
 
   if (style === "blue") {
     header.classList.remove("bg-color-white");
-    header.classList.add("bg-color-blue1");
+    header.classList.add("bg-color-blue");
 
-    logo.src = "src/imgs/logo_mobile_white.svg";
-    menu.src = "src/imgs/icon_menu_mobile_white.svg";
+    logoMobile.src = "src/imgs/header/logo_mobile_white.svg";
+    logoTablet.src = "src/imgs/header/logo_tablet_white.svg";
+    menu.src = "src/imgs/header/icon_menu_mobile_white.svg";
   }
 
   if (style === "white") {
-    header.classList.remove("bg-color-blue1");
+    header.classList.remove("bg-color-blue");
     header.classList.add("bg-color-white");
 
-    logo.src = "src/imgs/logo_mobile_blue.svg";
-    menu.src = "src/imgs/icon_menu_mobile_gray.svg";
+    logoMobile.src = "src/imgs/header/logo_mobile_blue.svg";
+    logoTablet.src = "src/imgs/header/logo_tablet_blue.svg";
+    menu.src = "src/imgs/header/icon_menu_mobile_gray.svg";
   }
 
   if (style === "modal") {
-    header.classList.remove("bg-color-blue1");
+    header.classList.remove("bg-color-blue");
     header.classList.add("bg-color-white");
 
-    logo.src = "src/imgs/logo_mobile_blue.svg";
-    menu.src = "src/imgs/icon_menu_mobile_close.svg";
+    logoMobile.src = "src/imgs/header/logo_mobile_blue.svg";
+    logoTablet.src = "src/imgs/header/logo_tablet_blue.svg";
+    menu.src = "src/imgs/header/icon_menu_mobile_close.svg";
   }
 };
 
