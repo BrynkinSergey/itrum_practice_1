9b0681d5d60869c8aff87192afc4be69e7b7e4b4
SergeyB <brynkin.dev@gmail.com>
2023-11-16 11:06:17 +0300

    add index.js changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,22 @@
+const blue2Color = "#192144";
+const whiteColor = "#fff";
+
 const setHeaderStyle = (style) => {
   const header = document.querySelector("header");
   const logoMobile = document.querySelector(".main-logo--mobile");
   const logoTablet = document.querySelector(".main-logo--tablet");
   const menu = document.querySelector(".icon-menu_mobile");
+  const headerNavbar = document.querySelector(".header_navbar");
+  const headerLogo = document.querySelector(".header_logo");
 
   if (style === "blue") {
     header.classList.remove("bg-color-white");
     header.classList.add("bg-color-blue");
 
+    headerNavbar.classList.remove("font-color-blue2");
+    headerNavbar.classList.add("font-color-white");
+
+    headerLogo.style.setProperty("--after-bg", whiteColor);
     logoMobile.src = "src/imgs/header/logo_mobile_white.svg";
     logoTablet.src = "src/imgs/header/logo_tablet_white.svg";
     menu.src = "src/imgs/header/icon_menu_mobile_white.svg";
@@ -17,6 +26,11 @@ const setHeaderStyle = (style) => {
     header.classList.remove("bg-color-blue");
     header.classList.add("bg-color-white");
 
+    headerNavbar.classList.remove("font-color-white");
+    headerNavbar.classList.add("font-color-blue2");
+
+    headerLogo.style.setProperty("--after-bg", blue2Color);
+
     logoMobile.src = "src/imgs/header/logo_mobile_blue.svg";
     logoTablet.src = "src/imgs/header/logo_tablet_blue.svg";
     menu.src = "src/imgs/header/icon_menu_mobile_gray.svg";
@@ -25,6 +39,11 @@ const setHeaderStyle = (style) => {
   if (style === "modal") {
     header.classList.remove("bg-color-blue");
     header.classList.add("bg-color-white");
+
+    headerNavbar.classList.remove("font-color-white");
+    headerNavbar.classList.add("font-color-blue2");
+
+    headerLogo.style.setProperty("--after-bg", blue2Color);
 
     logoMobile.src = "src/imgs/header/logo_mobile_blue.svg";
     logoTablet.src = "src/imgs/header/logo_tablet_blue.svg";
